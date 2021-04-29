ace65868c2107eb9b6143f99fcb66a7dfa0b14f9
Jocke <cajo1701@student.ju.se>
2021-04-29 13:54:24 +0100

    cloud connection

--- rbp/btcamera.py
+++ rbp/btcamera.py
@@ -2,20 +2,36 @@ import serial
 import time
 import picamera
 from time import sleep
+import requests
+ser = serial.Serial ("/dev/ttyS0", 9600)
 
-ser = serial.Serial ("/dev/ttyS0", 9600)    #Open port with baud rate
+URL = "https://us-central1-tigk-captain-america.cloudfunctions.net/Nodes"
+
+def sendData(data):
+    response = requests.post(URL, data)
+    print(response.status_code)
+print("Testing")
+newDrivePath = {"stringFromRbp": "0,0,S"}
+sendData(newDrivePath)
+
 while True:
     received_data = ser.read()              #read serial port
     sleep(0.03)
     data_left = ser.inWaiting()             #check for remaining byte
     received_data += ser.read(data_left)
     print (received_data)                   #print received data
-    if chr(received_data[-1]) == 'S':
+    sendDataString = {"stringFromRbp": received_data}
+    sendData(sendDataString)
+    if chr(received_data[-1]) == 'T':
         with picamera.PiCamera() as camera:
             camera.resolution = (1024, 768)
             camera.start_preview()
             # Camera warm-up time
 #             time.sleep()
             camera.capture('test.jpg')
-            
-            #ser.write(received_data)
+    elif chr(received_data[-1]) == 'F':
+        
+        print("tjo")
+    else:
+        print("fel")
+        
