d5bc12ba8429cdc9d88a0a07fa6b5f499820e318
Jocke <cajo1701@student.ju.se>
2021-05-21 09:28:44 +0100

    Renamed file and removed camera functionality

--- rbp/RPi_SendRecieve.py
+++ rbp/RPi_SendRecieve.py
@@ -1,9 +1,8 @@
 import serial
 import time
-import picamera
 from time import sleep
 import requests
-ser = serial.Serial ("/dev/ttyS0", 9600)
+ser = serial.Serial ("/dev/ttyS0", 115200)
 
 URL = "https://us-central1-tigk-captain-america.cloudfunctions.net/Nodes"
 
@@ -11,25 +10,18 @@ def sendData(data):
     response = requests.post(URL, data)
     print(response.status_code)
 print("Testing")
-newDrivePath = {"stringFromRbp": "0,0,S"}
+newDrivePath = {"stringFromRbp": "0,0,0,S"}
 sendData(newDrivePath)
-
+ser.write(b'R')                              #send to arduino that rpi is ready
 while True:
     received_data = ser.read()              #read serial port
     sleep(0.03)
     data_left = ser.inWaiting()             #check for remaining byte
     received_data += ser.read(data_left)
-    print (received_data)                   #print received data    
-    if chr(received_data[-1]) == 'T':
-        with picamera.PiCamera() as camera:
-            camera.resolution = (1024, 768)
-            camera.start_preview()
-            #Camera warm-up time
-            #time.sleep()
-            camera.capture('/home/pi/Desktop/robot/rbp/test.jpg')
-        sendDataString = {"stringFromRbp": received_data}
-        sendData(sendDataString)
-    elif chr(received_data[-1]) == 'F':
+    print (received_data)                   #print received data
+    if chr(received_data[0]) == 'R':
+        ser.write(b'R')
+    if chr(received_data[-1]) == 'T' or chr(received_data[-1]) == 'F':
         sendDataString = {"stringFromRbp": received_data}
         sendData(sendDataString)
     else:
